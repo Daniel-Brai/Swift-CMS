f8220413f44cf57d6e7c8a8e11a77afd84b4bf13
Daniel Brai <braidaniel2001@gmail.com>
2023-06-30 02:54:26 +0100

    chore(app): Added necessary dependencies

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   adapter: netlify(),
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
