80a2f08248b84d9b08b51736b4861d5890ab2538
Daniel Brai <braidaniel2001@gmail.com>
2023-07-27 18:35:50 +0100

    refactor(app): Started changes to the architecture

---
+++ app/src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,23 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost } from "@nestjs/common";
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter { 
+  catch(exception: HttpException, host: ArgumentsHost) { 
+    const ctx = host.switchToHttp();
+    const resposne = ctx.getResponse<Response>();
+    const status = exception = exception.getStatus();
+    const errorResponse = exception.getResponse();
+    
+    const context = { 
+      statusCode: status, 
+      error: errorResponse['error'],    
+      message: errorResponse['message'],
+      timestamp: formatTimestamp(Date.now()),
+    };
+
+    if (status === 401 ||  status === 403 || status === 404 || status === 500) {
+      response.render('error', context);
+    }
+  }
+}
--- app/src/main.ts
+++ app/src/main.ts
@@ -7,14 +7,12 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { createClient } from 'redis';
-import * as createRedisStore from 'connect-redis';
+import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
 import * as passport from 'passport';
-import * as session from 'express-session';
-import helmet from 'helmet';
 import * as express from 'express';
+import helmet from 'helmet';
 
 const logger: Logger = new Logger();
 
@@ -23,39 +21,14 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   const configService = app.get(ConfigService);
 
-  const PORT: number = +configService.get<number>('PORT') || 3000;
-  const RedisStore = createRedisStore(session);
-
-  const redisHost: string = configService.get<string>('REDIS_CLIENT');
-  const redisPort: number = +configService.get<number>('REDIS_PORT');
-  const redisClient = createClient({
-    host: redisHost,
-    port: redisPort,
-  });
-
-  redisClient.on('error', (e: any) => {
-    logger.error(`[Redis]: Redis connection failed: ${e.msg}...`);
-  });
-  redisClient.on('connect', () => {
-    logger.log('[Redis]: Redis connection successful...');
-  });
-
-  app.enableCors({});
-  app.use(
-    session({
-      store: new RedisStore({ client: redisClient }),
-      secret: `${configService.get<string>('SESSION_SECRET_KEY')}`,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3600000, httpOnly: true, sameSite: 'strict' },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
   app.use(cookieParser());
   app.use(helmet());
   app.use(compression());
   app.use(express.urlencoded({ extended: true }));
+
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '../client/www/root'));
+  app.setBaseViewsDir(join(__dirname, '../client/views'));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
   app.useGlobalPipes(
