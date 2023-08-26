a0eaf01ddc0357224de2ac481787e8cd4b5af0e1
Daniel Brai <braidaniel2001@gmail.com>
2023-08-26 18:50:57 +0100

    chore(app): Cleaned services and controllers

--- src/domain/auth/controller/auth.controller.ts
+++ src/domain/auth/controller/auth.controller.ts
@@ -9,6 +9,15 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthService } from '../services/auth.service';
 import { UserSignInDto } from '../dto/auth-request.dto';
 import { UserSignInResponseDto } from '../dto/auth-response.dto';
@@ -17,12 +26,32 @@ import { AccessTokenGuard } from '../guards/access-token.guard';
 import { GoogleOauthGuard } from '../guards/google-auth.guard';
 import { GithubOauthGuard } from '../guards/github-auth.guard';
 import { Response } from 'express';
+import {
+  NO_ENTITY_FOUND,
+  UNAUTHORIZED_REQUEST,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+} from '../../../app.constants';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Login user by email and password',
+    description: 'Login action returns user data, access and refresh token',
+  })
+  @ApiOkResponse({
+    description: 'User login successful',
+    type: UserSignInResponseDto,
+  })
+  @ApiInternalServerErrorResponse({
+    description: INTERNAL_SERVER_ERROR,
+  })
+  @ApiBadRequestResponse({ description: BAD_REQUEST })
+  @ApiConsumes('application/json')
   @Post('/login')
   public async CreateUser(
     @Body() body: UserSignInDto,
@@ -43,6 +72,11 @@ export class AuthController {
 
   @UseGuards(AccessTokenGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Logout user',
+    summary: 'Logout action removes access and refresh token',
+  })
+  @ApiConsumes('application/json')
   @Get('/logout')
   public async logout(@Req() _req: any, @Res() res: Response) {
     res.cookie('access_token', '', {
@@ -58,6 +92,11 @@ export class AuthController {
 
   @UseGuards(RefreshTokenGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Refresh user authentication token',
+    summary: 'Refresh action return new refresh token',
+  })
+  @ApiConsumes('application/json')
   @Post('/refresh')
   public async refreshToken(@Req() req: any) {
     const user = req.user;
@@ -66,11 +105,20 @@ export class AuthController {
 
   @UseGuards(GoogleOauthGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Login user using google',
+    summary: 'Login action returns google oauth content screen ',
+  })
   @Get('social/google/login')
   public async googleAuth(@Req() _req: any) {}
 
   @UseGuards(GoogleOauthGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Redirect callback for google auth',
+    summary:
+      'Callback action returns user data, access and refresh token and redirectes to the root url ',
+  })
   @Get('social/google/callback')
   public async googleAuthRedirect(@Req() req: any, @Res() res: Response) {
     const response = await this.authService.createSocialAuthToken(req.user);
@@ -87,11 +135,20 @@ export class AuthController {
 
   @UseGuards(GithubOauthGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Login user using google',
+    summary: 'Login action returns google oauth content screen ',
+  })
   @Get('social/github/login')
   public async githubAuth(@Req() _req: any) {}
 
   @UseGuards(GithubOauthGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Redirect callback for github auth',
+    summary:
+      'Callback action returns user data, access and refresh token and redirectes to the root url ',
+  })
   @Get('social/github/callback')
   public async githubAuthRedirect(@Req() req: any, @Res() res: Response) {
     const response = await this.authService.createSocialAuthToken(req.user);
@@ -106,3 +163,4 @@ export class AuthController {
     return res.redirect(`/`);
   }
 }
+
